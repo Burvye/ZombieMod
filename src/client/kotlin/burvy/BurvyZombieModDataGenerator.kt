e43df8b51e6edaead3e4ed234265d9ed24d9cb27
Burvye <thebuffonion@gmail.com>
2026-03-24 22:39:19 -0400

    Kotlin First

---
+++ src/client/kotlin/burvy/BurvyZombieModDataGenerator.kt
@@ -0,0 +1,9 @@
+package burvy
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BurvyZombieModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
